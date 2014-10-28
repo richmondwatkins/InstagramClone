61070e031e6a59f8b40ca345cd57a031b93d39ee
Richmond Watkins <richmondwatkins@mac.com>
2014-10-27 21:21:16 -0500

    camer view controller added

---
+++ CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  InstagramClone
+//
+//  Created by Richmond on 10/27/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ CameraViewController.m
@@ -0,0 +1,23 @@
+//
+//  CameraViewController.m
+//  InstagramClone
+//
+//  Created by Richmond on 10/27/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
